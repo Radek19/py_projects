073775b84f30f41cd621f9a228d5a8016d5a8952
Radek Pakosta <pakosta.radek@gmail.com>
2019-05-31 14:23:05 +0200

    graphene update

--- DjangoApi_project/graphapi/schema.py
+++ DjangoApi_project/graphapi/schema.py
@@ -25,8 +25,16 @@ class OrderItemsType(DjangoObjectType):
 
 
 class Query(object):
+    product = graphene.Field(ProductType,
+                             id=graphene.Int(),
+                             name=graphene.String())
     all_products = graphene.List(ProductType)
+
     all_orders = graphene.List(OrderType)
+    order = graphene.Field(OrderType,
+                           id=graphene.Int(),
+                           owner=graphene.String())
+
     all_orderitems = graphene.List(OrderItemsType)
 
     def resolve_all_products(self, info, **kwargs):
@@ -41,3 +49,27 @@ class Query(object):
 
         return OrderItems.objects.select_related('order').all()
         # return OrderItems.objects.all()
+
+    def resolve_product(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Product.objects.get(pk=id)
+
+        if name is not None:
+            return Product.objects.get(name=name)
+
+        return None
+
+    def resolve_order(self, info, **kwargs):
+        id = kwargs.get('id')
+        owner = kwargs.get('owner')
+
+        if id is not None:
+            return Order.objects.get(pk=id)
+
+        if name is not None:
+            return Order.objects.get(owner=owner)
+
+        return None
